3b56867aa3e31fec0a6572b60656cbff9d41731d
narhim <tmartin120492@gmail.com>
2023-07-23 16:28:47 +0200

    53.6% accuracy, no tokens are filtered

--- src/main.py
+++ src/main.py
@@ -28,104 +28,28 @@ def separate_language(list_of_lists):
 def tokenize_sentences(list_sentences):
 	corpus = []
 	for sentence in list_sentences:
-		sentence_tokenized = [c.lower() for c in sentence if not re.match('[.] |[.]{2,}|[:]|[;]|[\']|["]|[„]|[‟]|[”]|[<]+|[>]+|[(]|[)]|[\[]|[\]]|[¿]|[?]|[¡]|[!]',c)]
-		'''
-		sentence_tokenized = []
-		#Separate characters by delimiters defined in regex.
-		s = re.split(' |[.] |[.]{2,}|[:]|[;]|[\']|["]|[„]|[‟]|[”]|[<]+|[>]+|[(]|[)]|[\[]|[\]]|[¿]|[?]|[¡]|[!]',sentence)
-		#Initialize list in which results from strip will be appended.
-		result = []
-		for element in s:
-		#Remove characters specified in regex from string.
-			s_r = element.strip('\n|[.]+|[:]|[,]|[;]|[«]|[»]|["]|[„]|[‟]|[”]|[<]+|[>]+|[\*]|[(]|[)]|[\[]|[\]]|[¿]|[?]|[¡]|[!]')
-			result.append(s_r)
-		#Loop over results from strip for second clean up.
-		for w in result:
-			#Avoid empty strings and "===".
-			if (w=='') or (re.match('=+',w) != None):
-				continue
-			#Conditions for string with hyphens. And example for each condition is provided.
-			elif re.search('-',w) != None:
-				#5-5
-				if re.search('\d+-\d+',w):
-					numbers = re.split('-',w)
-					sentence_tokenized.append(numbers[0].lower())
-					sentence_tokenized.append(numbers[1].lower())
-			
-				#cat-dog
-				elif re.search('[a-z]+-[a-z]+',w,flags=re.IGNORECASE):
-					sentence_tokenized.append(w.lower())
-
-					#45-ben
-				elif re.search('\d+-\D+',w):
-					numbers = re.split('-',w)
-					sentence_tokenized.append(numbers[0].lower())
-					sentence_tokenized.append(numbers[1].lower())
-				#-32,9
-				elif re.match('-\d+',w):
-					sentence_tokenized.append(w)
-				#alt- (from (alt-)griechisch)
-				elif re.search('[a-z]-',w,flags=re.IGNORECASE):
-					clean = w.strip('-')
-					sentence_tokenized.append(clean.lower())
-				#-word
-				elif re.match('-\w+',w):
-					clean = w.strip('-')
-					sentence_tokenized.append(clean.lower())
-				#45-
-				elif re.match('\d+-',w):
-					clean = w.strip('-')
-					sentence_tokenized.append(clean.lower())
-				#-%45 and -«word
-				elif re.match('[-%\d+]|[-«\w+]',w):
-					clean = w.strip('[-%]|[-«]')
-					sentence_tokenized.append(clean.lower())
-				#Avoid "-".
-				elif re.match('^-$',w):
-					continue
-			elif (w == '/') or (w=="“"):
-				continue
-			elif re.search('[a-z]\/',w,flags=re.IGNORECASE):
-				splitted = w.split('/')
-				sentence_tokenized.append(splitted[0].lower())
-				sentence_tokenized.append(splitted[1].lower())
-			elif re.search('\d[.]\d',w):
-				sentence_tokenized.append(w.lower())
-			#avoid the second type of hyphen found in raw text.
-			elif re.match('–',w):
-					continue
-			#If none of the conditions above apply, simply lower and apply.
-			else:
-				sentence_tokenized.append(w.lower())
-			'''
+		#sentence_tokenized = [c.lower() for c in sentence if not re.match('[.] |[.]{2,}|[:]|[;]|[\']|["]|[„]|[‟]|[”]|[<]+|[>]+|[(]|[)]|[\[]|[\]]|[¿]|[?]|[¡]|[!]',c)]
+		sentence_tokenized = [c.lower() for c in sentence]
 		corpus.append(sentence_tokenized)
 	return corpus
 
-def build_bigrams(dict_sent_tok):
-	dict_big_sent = {lang : [] for lang in languages_list}
-	for language,sentences in dict_sent_tok.items():
-		for s in sentences:
-			for n,word in enumerate(s):
-				if n > 0:
-					dict_big_sent[language].append((s[n-1],word))
-	return dict_big_sent
-def build_trigrams(dict_sent_tok):
-	dict_trig_sent = {lang : [] for lang in languages_list}
+def build_unigrams(dict_sent_tok):
+	dict_unig_sent = {lang : [] for lang in languages_list}
 	for language,sentences in dict_sent_tok.items():
 		for s in sentences:
 			for n,word in enumerate(s):
 				#if n > 1:
-				dict_trig_sent[language].append(word)
-	return dict_trig_sent
+				dict_unig_sent[language].append(word)
+	return dict_unig_sent
 
-def build_tetragrams(dict_sent_tok):
-	dict_tetra_sent = {lang : [] for lang in languages_list}
+def build_trigrams(dict_sent_tok):
+	dict_tri_sent = {lang : [] for lang in languages_list}
 	for language,sentences in dict_sent_tok.items():
 		for s in sentences:
 			for n,word in enumerate(s):
 				if n > 0:
-					dict_tetra_sent[language].append((s[n-1],word))
-	return dict_tetra_sent
+					dict_tri_sent[language].append((s[n-1],word))
+	return dict_tri_sent
 
 def count_ngrams(dict_ngrams,len_dict):
 	dict_freq_ngram = dict.fromkeys(languages_list)
@@ -145,33 +69,33 @@ def main():
 	sen_lan_list = read_file("data/train_dev/train.txt") #Extract data [[sentence,language]]
 	lang_sens_dict = separate_language(sen_lan_list) #Separate by language {language:[s1,s2]}
 	lang_nsens_dict = {lang:len(sentences) for lang,sentences in lang_sens_dict.items()} #Compute number of sentences per language {language:#sentences}
-	tri_except_dict = {language:(1/n) for language,n in lang_nsens_dict.items()} #Smoothing values for initial states {language:1/#sentences}
+	uni_except_dict = {language:(1/n) for language,n in lang_nsens_dict.items()} #Smoothing values for initial states {language:1/#sentences}
 	lang_senstok_dict = {}#Tokenize by letters {language:[[l,e,t, ,t,e,r,s],[l,e,t, ,t,e,r,s]]}
 	for language,sentences in lang_sens_dict.items():
 		corpus_list = tokenize_sentences(sentences)
 		lang_senstok_dict[language] = corpus_list
+	lang_uni_dict = build_unigrams(lang_senstok_dict)#Build unigrams {lang:[ngram,ngram]}
 	lang_tri_dict = build_trigrams(lang_senstok_dict)#Build trigrams {lang:[ngram,ngram]}
-	lang_tetra_dict = build_tetragrams(lang_senstok_dict)#Build tetragrams {lang:[ngram,ngram]}
-	abs_tri_freq_dict = count_ngrams(lang_tri_dict,lang_nsens_dict) #Compute abs frequencies{lang:[{ngram:freq},{ngram:freq}]}
-	tri_freq_dict = {}#Transform into initial state frequencies {lang:[{ngram:freq},{ngram:freq}]}
-	for language,ngrams in abs_tri_freq_dict.items():
+	abs_uni_freq_dict = count_ngrams(lang_uni_dict,lang_nsens_dict) #Compute abs frequencies{lang:[{ngram:freq},{ngram:freq}]}
+	uni_freq_dict = {}#Transform into initial state frequencies {lang:[{ngram:freq},{ngram:freq}]}
+	for language,ngrams in abs_uni_freq_dict.items():
 		nsens = lang_nsens_dict[language]
 		clean_ngrams = {}
 		for n,f in ngrams.items():
 			clean_ngrams[n] = (f+1)/nsens
-		tri_freq_dict[language] = clean_ngrams
+		uni_freq_dict[language] = clean_ngrams
 	
-	abs_tetra_freq_dict = count_ngrams(lang_tetra_dict,lang_nsens_dict)#Compute abs frequencies{lang:[{ngram:freq},{ngram:freq}]}
-	tetra_freq_dict = {}#Transform into initial state frequencies {lang:[{ngram:freq},{ngram:freq}]}
-	for language,ngrams in abs_tetra_freq_dict.items():
-		ntetragrams = len(ngrams.keys())
+	abs_tri_freq_dict = count_ngrams(lang_tri_dict,lang_nsens_dict)#Compute abs frequencies{lang:[{ngram:freq},{ngram:freq}]}
+	tri_freq_dict = {}#Transform into initial state frequencies {lang:[{ngram:freq},{ngram:freq}]}
+	for language,ngrams in abs_tri_freq_dict.items():
+		ntrigrams = len(ngrams.keys())
 		clean_ngrams = {}
 		for n,f in ngrams.items():
-			clean_ngrams[n] = (f+1)/ntetragrams
-		tetra_freq_dict[language] = clean_ngrams
-	tetra_except_dict = {lang:(1/len(v)) for lang,v in abs_tetra_freq_dict.items()}
+			clean_ngrams[n] = (f+1)/ntrigrams
+		tri_freq_dict[language] = clean_ngrams
+	tri_except_dict = {lang:(1/len(v)) for lang,v in abs_tri_freq_dict.items()}
+
 	#Dev
-	
 	dev_sen_lan_list = read_file("data/train_dev/devel.txt") #Extract data
 	gold_labels_list = [pair[1] for n,pair in enumerate(dev_sen_lan_list)]
 	id_sentence_dict = {n:pair[0] for n,pair in enumerate(dev_sen_lan_list)}
@@ -179,51 +103,50 @@ def main():
 	dev_corpus = tokenize_sentences(dev_sentences_list)
 	results = []
 	for sentence in dev_corpus:
-		trigrams = []
+		unigrams = []
 		for n,word in enumerate(sentence):
 			#if n > 1:
-			trigrams.append(word)
-		tetragrams = []
+			unigrams.append(word)
+		trigrams = []
 		for n,word in enumerate(sentence):
 			if n > 0:
-				tetragrams.append((sentence[n-1],word))
+				trigrams.append((sentence[n-1],word))
 		language_probability_dict = {}
-		for language,ngrams in tri_freq_dict.items():
+		for language,ngrams in uni_freq_dict.items():
 			probabilities = []
 			try:
-				initial_prob = ngrams[trigrams[0]]
+				initial_prob = ngrams[unigrams[0]]
 				probabilities.append(initial_prob)
 			except:
-				initial_prob = tri_except_dict[language]
+				initial_prob = uni_except_dict[language]
 				probabilities.append(initial_prob)
-			for t in tetragrams:
+			for t in trigrams:
 				try:
-					transitional_prob = tetra_freq_dict[language][t]
+					transitional_prob = tri_freq_dict[language][t]
 					probabilities.append(transitional_prob)
 				except:
-					#continue
-					transitional_prob = tetra_except_dict[language]
+					transitional_prob = tri_except_dict[language]
 					probabilities.append(transitional_prob)
 			prob = math.prod(probabilities)
 			language_probability_dict[language] = prob
-		try:
-			majority = list(language_probability_dict.keys())[0]
-			probability = list(language_probability_dict.values())[0]
-			for lang,prob in language_probability_dict.items():
-				if prob > probability:
-					majority = lang
-					probability = prob
-			results.append(majority) 
-		except:
-			results.append("NaN")
+		#try:
+		majority = list(language_probability_dict.keys())[0]
+		probability = list(language_probability_dict.values())[0]
+		for lang,prob in language_probability_dict.items():
+			if prob > probability:
+				majority = lang
+				probability = prob
+		results.append(majority) 
+		#except:
+		#	results.append("NaN")
 
 	total_sentences = len(dev_corpus)
 	nas = 0
 	accuracy = 0
 	for g,r in zip(gold_labels_list,results):
-		if r == "NaN":
-			nas += 1
-		elif r==g:
+		#if r == "NaN":
+		#	nas += 1
+		if r==g:
 			accuracy += 1 
 	nas = nas*100/total_sentences
 	print(nas)
@@ -239,25 +162,25 @@ def main():
 	language_sen_maj = {language:[] for language in languages_list}
 	for l,sentences in test_lang_senstok_dict.items():
 		for sentence in sentences:
-			trigrams = []
+			unigrams = []
 			for n,word in enumerate(sentence):
 				if n > 1:
-					trigrams.append((sentence[n-2],sentence[n-1],word))
-			tetragrams = []
+					unigrams.append((sentence[n-2],sentence[n-1],word))
+			trigrams = []
 			for n,word in enumerate(sentence):
 				if n > 2:
-					tetragrams.append((sentence[n-3],sentence[n-2],sentence[n-1],word))
+					trigrams.append((sentence[n-3],sentence[n-2],sentence[n-1],word))
 			language_probability_dict = {}
-			for language,ngrams in tri_freq_dict.items():
+			for language,ngrams in uni_freq_dict.items():
 				probabilities = []
 				try:
-					initial_prob = ngrams[trigrams[0]]
+					initial_prob = ngrams[unigrams[0]]
 					probabilities.append(initial_prob)
 				except:
 					continue
-				for t in tetragrams:
+				for t in trigrams:
 					try:
-						transitional_prob = tetra_freq_dict[language][t]
+						transitional_prob = tri_freq_dict[language][t]
 						probabilities.append(transitional_prob)
 					except:
 						continue
